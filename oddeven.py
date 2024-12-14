a0816d01b4a9c5ad5aedecb3c1414f88987461af
ektajha-18 <ektacatalyseur@gmail.com>
2024-12-14 06:34:38 +0530

    Done

--- oddeven.py
+++ oddeven.py
@@ -1,5 +1,7 @@
-number=int(input("Enter the number:"))
-if number%2==0:
-    print(number," is an even number")
+sale_amount=float(input("Enter the selling price: "))
+cost_price=float(input("Enter the cost price: "))
+if sale_amount>cost_price:
+    profit_value=sale_amount-cost_price
+    print("Its a profit, The profit amount is ", profit_value)
 else:
-    print(number," is a odd number")
+    print("Its a loss")
